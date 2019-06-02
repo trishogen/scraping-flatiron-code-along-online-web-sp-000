ee0668ba2fabd068d28ccfea32f9519543f0329a
Trish Ogen <patriciaogen@gmail.com>
2019-06-02 18:04:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,11 @@ class Scraper
     # binding.pry
   end
 
-    def print_courses
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
