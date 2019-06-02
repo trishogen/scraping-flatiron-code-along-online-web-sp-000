6b2c6c21c94037af5a122c77e4c01237b1b55b5b
Trish Ogen <patriciaogen@gmail.com>
2019-06-02 17:48:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
 
     def print_courses
@@ -21,3 +22,5 @@ class Scraper
   end
 
 end
+
+Scraper.new.get_page
